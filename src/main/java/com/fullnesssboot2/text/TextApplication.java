c509fc51772858ff01a6bdd2f41f2e74829f6f49
高橋良輔 <r-takahashi@image-pit.com>
2020-06-01 14:19:26 +0900

    インジェクションの例実装

--- src/main/java/com/fullnesssboot2/text/TextApplication.java
+++ src/main/java/com/fullnesssboot2/text/TextApplication.java
@@ -1,13 +1,26 @@
 package com.fullnesssboot2.text;
 
+import com.fullnesssboot2.text.interfacedepnd.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TextApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(TextApplication.class, args);
+		// 1. SpringBoot実行結果をApplicatinContext型の変数に入れる
+		ApplicationContext context = SpringApplication.run(TextApplication.class, args);
+		// 4. これでSpringBoot起動後の処理を追加できる
+		context.getBean(TextApplication.class).run();
 	}
+	// 3. DI（インジェクション）
+	@Autowired // <-自動的にDIコンテナがインスタンス生成してくれる
+	Calculator calculator;
 
+	// 2. 追加で動かしたいメソッドを定義
+	public void run(){
+		// 5. インスタンスを使って処理
+		System.out.println("計算結果:"+calculator.calc(1,4));
+	}
 }
--- src/main/java/com/fullnesssboot2/text/interfacedepnd/AddCalcu.java
+++ src/main/java/com/fullnesssboot2/text/interfacedepnd/AddCalcu.java
@@ -1,6 +1,9 @@
 package com.fullnesssboot2.text.interfacedepnd;
 
+import org.springframework.stereotype.Component;
+
 // 足し算をする目的の実装クラス
+@Component // <-これでDI対象になる
 public class AddCalcu implements Calculator {
     @Override
     public Integer calc(Integer x, Integer y) {
