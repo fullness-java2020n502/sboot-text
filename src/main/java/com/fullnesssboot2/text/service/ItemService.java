2a15b649f9191ddbe1e60d612e5e23692f5ad54e
高橋良輔 <r-takahashi@image-pit.com>
2020-06-03 15:57:38 +0900

    Spring MVC演習1と2の解答例

--- src/main/java/com/fullnesssboot2/text/service/ItemService.java
+++ src/main/java/com/fullnesssboot2/text/service/ItemService.java
@@ -1,4 +1,7 @@
 package com.fullnesssboot2.text.service;
 
+import com.fullnesssboot2.text.entity.Item;
+
 public interface ItemService {
+    void hurikomi(Item item1, Item item2);
 }
--- src/main/java/com/fullnesssboot2/text/service/ItemServiceImpl.java
+++ src/main/java/com/fullnesssboot2/text/service/ItemServiceImpl.java
@@ -1,4 +1,20 @@
 package com.fullnesssboot2.text.service;
 
-public class ItemServiceImpl {
+import com.fullnesssboot2.text.entity.Item;
+import com.fullnesssboot2.text.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ItemServiceImpl implements ItemService{
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Override
+    @Transactional
+    public void hurikomi(Item item1, Item item2) {
+        itemRepository.create(item1);
+        itemRepository.create(item2);
+    }
 }
