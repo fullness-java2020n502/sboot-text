602564781c3263a8f2fe070ce56104e5fb3cc0b4
高橋良輔 <r-takahashi@image-pit.com>
2020-06-04 16:00:32 +0900

    SessionControllerにもバリデート処理を追加

--- src/main/java/com/fullnesssboot2/text/controller/SessionController.java
+++ src/main/java/com/fullnesssboot2/text/controller/SessionController.java
@@ -1,6 +1,11 @@
 package com.fullnesssboot2.text.controller;
 
+import com.fullnesssboot2.text.entity.Item;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -20,7 +25,12 @@ public class SessionController {
     }
     // 確認画面
     @PostMapping("confirm")
-    public String confirm(@ModelAttribute("empForm") ValueForm form){
+    public String confirm(
+            @Validated @ModelAttribute("empForm") ValueForm form,
+            BindingResult result // <-入力チェックを実施した結果を取得
+    ){
+        // エラー時に入力画面に戻す
+        if(result.hasErrors()) return "session/form";
         return "session/confirm";
     }
     // 完了画面
@@ -28,6 +38,8 @@ public class SessionController {
     public String complete(@ModelAttribute("empForm") ValueForm form, SessionStatus sessionStatus){
         System.out.println(form);
         // TODO formのデータを使ってインサート処理
+        ModelMapper modelMapper = new ModelMapper();
+        Item item = modelMapper.map(form, Item.class);
         // セッション解除
         sessionStatus.setComplete();
         return "session/complete";
--- src/main/java/com/fullnesssboot2/text/controller/ValueForm.java
+++ src/main/java/com/fullnesssboot2/text/controller/ValueForm.java
@@ -2,12 +2,16 @@ package com.fullnesssboot2.text.controller;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
 public class ValueForm implements Serializable {
+    @Max(10)
     private Integer intValue;
+    @NotBlank
     private String stringValue;
+    @Future
     private Date dateValue;
 }
