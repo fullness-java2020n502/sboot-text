c32d1b10e1fde83b8ef4e157002490ee68adaa42
Ryosuke Takahashi <r-takahashi@image-pit.com>
2020-06-18 08:36:55 +0900

    Thymeleafのインクルードサンプル

---
+++ src/main/java/com/fullnesssboot2/text/controller/LayoutController.java
@@ -0,0 +1,14 @@
+package com.fullnesssboot2.text.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/layout")
+@Component
+public class LayoutController {
+    @GetMapping
+    public String layout(){
+        return "include/content";
+    }
+}
